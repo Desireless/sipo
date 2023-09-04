dac2775bba219dabfafe2c1193931c256bdd80a3
Juan Bastias <mainjpbb@gmail.com>
2023-09-03 23:37:35 -0300

    refactor (Follow Button Component): Modifica logica para determinar follow o unfollow de un usuario, agrega parametro is_following al action followAction. Agrega estado isLoading que deshabilita el boton Follow Button mientras espera la respuesta del Action.

--- src/app/components/follow-button-action.ts
+++ src/app/components/follow-button-action.ts
@@ -1,10 +1,15 @@
 'use server';
-
 import { getErrorMessage } from "@/utils";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export const followUser = async (user_id_to_follow: string) => {
+
+/*
+*   Funcion para seguir o dejar de seguir a un usuario
+*   @param user_id_to_follow: ID del usuario a seguir
+*   @param is_follwing: booleano que indica si se quiere seguir o dejar de seguir
+*/
+export const followAction = async (user_id_to_follow: string, is_follwing: boolean) => {
     const supabase = createServerComponentClient<Database>({ cookies });
     const { data: { session } } = await supabase.auth.getSession();
 
@@ -14,19 +19,24 @@ export const followUser = async (user_id_to_follow: string) => {
 
 
     try {
-        const { data, error } = await supabase
-            .from('followers')
-            .insert([
-                { follower_id: session?.user.id, following_id: user_id_to_follow },
-            ])
-            .select()
+        const { data, error } = is_follwing ?
+            await supabase
+                .from('followers')
+                .delete()
+                .match({ follower_id: session?.user.id, following_id: user_id_to_follow })
+            : await supabase
+                .from('followers')
+                .insert([
+                    { follower_id: session?.user.id, following_id: user_id_to_follow },
+                ])
+                .select();
 
 
         if (error) {
             return { error: getErrorMessage(error) }
         }
 
-        if (data.length === 0) {
+        if (data?.length === 0) {
             return { error: "No hubo respuesta del servidor" }
         }
 
--- src/app/components/follow-button.tsx
+++ src/app/components/follow-button.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import React, { useState } from 'react'
-import { followUser } from './follow-button-action';
+import { followAction } from './follow-button-action';
 import { ToastService } from '@/services';
+import { useRouter } from 'next/navigation';
 
 type FollowButtonProps = {
     userId: string;
@@ -11,27 +12,34 @@ type FollowButtonProps = {
 
 export default function FollowButton( { userId, isFollower }: FollowButtonProps ) {
     const [isFollowing, setIsFollowing] = useState(isFollower);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const router = useRouter();
 
     const handleFollow = async () => {
-        const result = await followUser(userId);
+        setIsLoading(true);
+
+        const result = await followAction(userId, isFollowing);
 
         if (result?.error) {
 			ToastService.error(result.error)
+            setIsLoading(false);
 		} else {
-			ToastService.success("Siguiendo");
             setIsFollowing(!isFollowing);
+            setIsLoading(false);
+            router.refresh();
 		}
 
     }
 
     return (
         <div className='flex justify-center max-w-full'>
-            <button className=" bg-black rounded-lg transition duration-200 hover:bg-slate-700 px-4 py-1 text-white font-semibold"
-                onClick={handleFollow}
+            <button
+            className=" bg-black rounded-lg transition duration-200 hover:bg-slate-700 px-4 py-1 text-white font-semibold disabled:opacity-25"
+            onClick={handleFollow}
+            disabled={isLoading}
             >
-
                 {isFollowing ? 'Unfollow' : 'Follow'}
-
             </button>
         </div>
     )
